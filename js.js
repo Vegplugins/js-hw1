859c1fc749c7a53a281089bd04d7698b44f40a56
paskuda <82751857+Vegplugins@users.noreply.github.com>
2025-07-17 13:55:54 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+const age = 14;
+
+const name = "Мiша";
+
+const isStudent = true;
+
+const myString = "улюблена цитата";
+
+let myNumber = 5;
+
+myNumber += 10;
+
+const myNull = null;
+
+console.log(
+  `${age}, ${name}, ${isStudent}, ${myString}, ${myNumber}, ${myNull}`
+);
+
+const userName = prompt("Введіть своє ім’я");
+alert(userName);
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+confirm("Увага! Дія може бути небезпечною. Ви впевнені, що хочете продовжити?");
